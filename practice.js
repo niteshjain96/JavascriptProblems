e1dd88ec88454366aac3d86ead468461329dd9ef
niteshjain96 <niteshjain5432@gmail.com>
2024-05-23 22:52:51 +0530

    js

--- practice.js
+++ practice.js
@@ -1,11 +1,25 @@
-// longest word
+// function mul(x){
+//     return function(y){
+//         return function(z){
+//             return x*y*z;
+//         }
+//     }
+// }
 
+// console.log(mul(10)(20)(30))
 
-function longestword(str){
-    let arr=str.split(' ');
-    
-    for(let i=0;i<arr.length;i++){
-        let word=arr[i].length;
-        if(arr[i])
+function sort(arr){
+    for(let i=0;i<arr.length-1;i++){
+        for(let j=0;j<arr.length-1-i;j++){
+            if(arr[j]>arr[j+1]){
+                // swap
+                let temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        }
     }
+    return arr;
 }
+let arr=[20,10,74,46,18];
+console.log(sort(arr));
